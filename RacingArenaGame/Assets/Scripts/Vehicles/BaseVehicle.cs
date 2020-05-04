ca96556a260d296c886fedd61139a04c9f924c34
Chubblezap <ajwiley@ucsc.edu>
2020-05-04 16:00:01 -0700

    Basic Controls
    -Stat-based modular controls, should tune more later.
    -Boost UI

--- RacingArenaGame/Assets/Scripts/Vehicles/BaseVehicle.cs
+++ RacingArenaGame/Assets/Scripts/Vehicles/BaseVehicle.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
 
 public class BaseVehicle : MonoBehaviour
@@ -10,16 +11,21 @@ public class BaseVehicle : MonoBehaviour
     public float BaseAcceleration;
     public float BaseTurn;
     public float BaseBoost; //BWAAAAAAAAAAAA
-    public float BaseWeight;
+    public float BaseArmor;
     public float BaseOffense;
-    public float BaseDefense;
+    public float BaseAir;
+
+    //UI Elements
+    public GameObject ChargeBar;
+    public GameObject Speedometer;
 
     //Stat modifiers
     [HideInInspector]
-    public float ModTopSpeed, ModAcceleration, ModTurn, ModBoost, ModWeight, ModOffense, ModDefense;
+    public float ModTopSpeed, ModAcceleration, ModTurn, ModBoost, ModArmor, ModOffense, ModDefense, ModAir;
 
     public PlayerInput controls;
     private float isHolding;
+    private float currentCharge;
 
     // Start is called before the first frame update
     void Start()
@@ -30,21 +36,27 @@ public class BaseVehicle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        isHolding = controls.actions["Hold"].ReadValue<float>();
+        ChargeBar.GetComponent<Image>().fillAmount = currentCharge;
     }
 
     void FixedUpdate() //put movement/physics stuff here
     {
-        isHolding = controls.actions["Hold"].ReadValue<float>();
-        Turn(controls.actions["Move"].ReadValue<Vector2>().x);
+        Turn(controls.actions["Move"].ReadValue<Vector2>().x, BaseTurn, ModTurn);
         if (isHolding == 1) // player is holding Space or A
         {
-            Charge();
+            Charge(BaseArmor, ModArmor, BaseBoost, ModBoost);
         }
         else
         {
-            Accelerate();
+            if(currentCharge >= 1)
+            {
+                Boost(BaseBoost, ModBoost);
+            }
+            currentCharge = 0;
+            Accelerate(BaseAcceleration, ModAcceleration, BaseTopSpeed, ModTopSpeed);
         }
+        DoDrag(BaseArmor, ModArmor);
     }
 
     void Init()
@@ -53,38 +65,47 @@ public class BaseVehicle : MonoBehaviour
         ModAcceleration = 0;
         ModTurn = 0;
         ModBoost = 0;
-        ModWeight = 0;
+        ModArmor = 0;
         ModOffense = 0;
-        ModDefense = 0;
+        ModAir = 0;
         isHolding = 0;
+        currentCharge = 0;
         controls = GetComponent<PlayerInput>();
     }
 
-    void Turn(float direction) //direction is a value between -1 and 1
+    void Turn(float direction, float bTurn, float mTurn) //direction is a value between -1 and 1
     {
-
+        transform.Rotate(0, direction * 0.25f * (bTurn + mTurn), 0, Space.Self);
     }
 
-    void Accelerate()
+    void Accelerate(float bAcceleration, float mAcceleration, float bTopSpeed, float mTopSpeed)
     {
         Rigidbody body = GetComponent<Rigidbody>();
-        body.AddForce(transform.forward * (BaseAcceleration + ModAcceleration));
-        if(body.velocity.sqrMagnitude > (BaseTopSpeed + ModTopSpeed))
+        body.AddForce(transform.forward * (bAcceleration + mAcceleration));
+        if(body.velocity.magnitude > (bTopSpeed + mTopSpeed))
         {
-            body.velocity *= .99f;
+            body.velocity *= .96f;
         }
     }
 
-    void Charge()
+    void Boost(float bBoost, float mBoost)
     {
         Rigidbody body = GetComponent<Rigidbody>();
-        if (body.velocity.sqrMagnitude < 0.1)
-        {
-            body.velocity *= .99f;
-        }
-        else
-        {
-            body.AddForce(transform.forward * -(BaseWeight + ModWeight));
-        }
+        body.AddForce(transform.forward * 50 * (bBoost + mBoost));
+    }
+
+    void Charge(float bArmor, float mArmor, float bBoost, float mBoost)
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        body.velocity *= (1f - (0.003f*(bArmor + mArmor)));
+        currentCharge += 0.001f * (bBoost + mBoost);
+    }
+
+    void DoDrag(float bArmor, float mArmor)
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        Vector3 localVelocity = body.transform.InverseTransformDirection(body.velocity);
+        localVelocity.x *= 1f - (0.001f*(bArmor + mArmor)); // lower sideways speed
+        body.velocity =  body.transform.TransformDirection(localVelocity);
     }
 }
