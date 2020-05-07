9c372954663eded6da929e0d90f175886ff7d405
Chubblezap <ajwiley@ucsc.edu>
2020-05-07 16:11:34 -0700

    Outline Shader + Particles

--- RacingArenaGame/Assets/Scripts/Utilities/HeldGun.cs
+++ RacingArenaGame/Assets/Scripts/Utilities/HeldGun.cs
@@ -6,12 +6,21 @@ public class HeldGun : MonoBehaviour
 {
     public Transform owner;
     public string gunType;
-    
+    private float curSpeed = 15;
+
+    void Update()
+    {
+        if(curSpeed <= 100)
+        {
+            curSpeed += Time.deltaTime * 2;
+        }
+    }
+
     void FixedUpdate()
     {
         if(owner != null)
         {
-            transform.position = Vector3.MoveTowards(transform.position, owner.position + new Vector3(0, 1, 0), 100 * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, owner.position + new Vector3(0, 1, 0), curSpeed * Time.deltaTime);
         }
     }
 }
