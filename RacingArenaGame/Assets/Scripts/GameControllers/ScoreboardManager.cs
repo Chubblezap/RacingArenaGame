43444e9def8c9dfbbca01b45b11ea9beef8740fd
Chubblezap <ajwiley@ucsc.edu>
2020-11-27 15:56:27 -0800

    Full Loop
    need to fix some stuff with event timings and ammo

--- RacingArenaGame/Assets/Scripts/GameControllers/ScoreboardManager.cs
+++ RacingArenaGame/Assets/Scripts/GameControllers/ScoreboardManager.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScoreboardManager : MonoBehaviour
 {
     private ScoreboardData scoredata;
+    private bool quittable = false;
     public GameObject scoresheet;
 
     // Start is called before the first frame update
@@ -12,13 +14,18 @@ public class ScoreboardManager : MonoBehaviour
     {
         scoredata = GameObject.Find("ScoreboardData(Clone)").GetComponent<ScoreboardData>();
         AddScores(scoredata);
-        GameObject.Find("Fader").GetComponent<Fader>().doFadeOut(1f);
+        GameObject.Find("Fader").GetComponent<Fader>().doFadeOut(0.5f);
+        StartCoroutine("ScoreDrops");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(quittable && Input.GetButtonDown(scoredata.leadingPlayer.GetComponent<Player>().startInput))
+        {
+            Mango();
+            SceneManager.LoadScene("MainMenu");
+        }
     }
 
     void AddScores(ScoreboardData s)
@@ -27,5 +34,27 @@ public class ScoreboardManager : MonoBehaviour
         {
             scoresheet.transform.GetChild(i).GetComponent<ScoreboardItem>().SetScore(s.playerOrder[i].playerNum, s.playerScoreString[i]);
         }
+    }
+
+    private IEnumerator ScoreDrops()
+    {
+        yield return new WaitForSecondsRealtime(0.5f);
+        for(int i = 3; i >= 0; i--)
+        {
+            scoresheet.transform.GetChild(i).GetComponent<ScoreboardItem>().StartCoroutine("DropIn");
+            yield return new WaitForSecondsRealtime(0.5f);
+        }
+        GameObject.Find("Fader").GetComponent<Fader>().doFadeIn(0.1f);
+        yield return new WaitForSecondsRealtime(0.1f);
+        GameObject.Find("Fader").GetComponent<Fader>().doFadeOut(3f);
+        yield return new WaitForSecondsRealtime(3f);
+        quittable = true;
+        yield return null;
+    }
+
+    private void Mango() //blow up
+    {
+        Destroy(GameObject.Find("ScoreboardData(Clone)"));
+        Destroy(GameObject.Find("DataCarrier(Clone)"));
     }
 }
--- RacingArenaGame/Assets/Scripts/UI/ScoreboardItem.cs
+++ RacingArenaGame/Assets/Scripts/UI/ScoreboardItem.cs
@@ -10,4 +10,34 @@ public class ScoreboardItem : MonoBehaviour
         transform.GetChild(0).GetComponent<Text>().text = "P" + playernum.ToString();
         transform.GetChild(1).GetComponent<Text>().text = scoretext;
     }
+
+    public void SetAlpha(float a) // a = 0-1
+    {
+        GetComponent<Image>().color = new Color(1, 1, 1, a);
+        for(int i=0; i<transform.childCount; i++)
+        {
+            if(transform.GetChild(i).GetComponent<Text>() != null)
+            {
+                transform.GetChild(i).GetComponent<Text>().color = new Color(0.2f, 0.2f, 0.2f, a);
+            }
+            else if (transform.GetChild(i).GetComponent<Image>() != null)
+            {
+                transform.GetChild(i).GetComponent<Image>().color = new Color(1, 1, 1, a);
+            }
+        }
+    }
+
+    public IEnumerator DropIn()
+    {
+        float timer = 0;
+        Vector3 initSize = new Vector3(1, 1, 1);
+        Vector2 bigSize = new Vector3(3, 3, 1);
+        while (timer < 0.5f)
+        {
+            GetComponent<RectTransform>().localScale = Vector3.Lerp(bigSize, initSize, timer / 0.5f);
+            SetAlpha(timer * 2);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
 }
